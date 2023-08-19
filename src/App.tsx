9b7e5f862e4cea5cc877ede4c09c17fd5b00aaab
nickIntagly <nicholasoliveira@intagly.com>
2023-08-19 14:35:24 -0700

    Add keyboard and start styling

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,9 @@ function App() {
         </div>
         <HangmanDrawing />
         <HangmanWord />
-        <Keyboard />
+        <div style={{ alignSelf: "stretch" }}>
+          <Keyboard />
+        </div>
       </div>
     </>
   );
