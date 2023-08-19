4d312c12cb68e3580997f9709054c34cd6e30992
nickIntagly <nicholasoliveira@intagly.com>
2023-08-19 14:06:29 -0700

    Finish hangman body parts

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -12,10 +12,88 @@ const HEAD = (
   ></div>
 );
 
+const BODY = (
+  <div
+    style={{
+      width: "10px",
+      height: "100px",
+      backgroundColor: "black",
+      position: "absolute",
+      top: "120px",
+      right: 0,
+    }}
+  ></div>
+);
+
+const RIGHT_ARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      backgroundColor: "black",
+      position: "absolute",
+      top: "150px",
+      right: "-100px",
+      rotate: "-30deg",
+      transformOrigin: "left bottom",
+    }}
+  ></div>
+);
+
+const LEFT_ARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      backgroundColor: "black",
+      position: "absolute",
+      top: "150px",
+      right: "10px",
+      rotate: "30deg",
+      transformOrigin: "right bottom",
+    }}
+  ></div>
+);
+
+const RIGHT_LEG = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      backgroundColor: "black",
+      position: "absolute",
+      top: "210px",
+      right: "-100px",
+      rotate: "30deg",
+      transformOrigin: "left top",
+    }}
+  ></div>
+);
+
+const LEFT_LEG = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      backgroundColor: "black",
+      position: "absolute",
+      top: "210px",
+      right: "10px",
+      rotate: "-30deg",
+      transformOrigin: "right top",
+    }}
+  ></div>
+);
+
 export function HangmanDrawing() {
   return (
     <div style={{ position: "relative" }}>
       {HEAD}
+      {BODY}
+      {RIGHT_ARM}
+      {LEFT_ARM}
+      {RIGHT_LEG}
+      {LEFT_LEG}
       {/* Top bar */}
       <div
         style={{
