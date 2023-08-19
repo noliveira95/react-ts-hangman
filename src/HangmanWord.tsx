83735d68f371773086d8eb993e9266291e76b1b4
nickIntagly <nicholasoliveira@intagly.com>
2023-08-19 14:24:19 -0700

    Add word and set up visibility conditions

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,1 +1,32 @@
-export function HangmanWord() {}
+export function HangmanWord() {
+  const word = "test";
+  const guessedLetters = ["t"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        fontFamily: "sans-serif",
+        textTransform: "uppercase",
+      }}
+    >
+      {word.split("").map((letter, index) => {
+        return (
+          <span style={{ borderBottom: ".1em solid black" }} key={index}>
+            <span
+              style={{
+                visibility: guessedLetters.includes(letter)
+                  ? "visible"
+                  : "hidden",
+              }}
+            >
+              {letter}
+            </span>
+          </span>
+        );
+      })}
+    </div>
+  );
+}
